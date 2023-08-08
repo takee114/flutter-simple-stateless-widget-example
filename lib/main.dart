7ea9833c2fd3c46d94a5bb7e8eb2e4b89e3f3721
takee114 <tekaligngetachew114@mail.com>
2023-08-08 11:19:35 -0700

    forget and sign up added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,14 +9,84 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        body: Material(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        appBar: AppBar(
+          title: Text("welcome"),
+          shadowColor: Colors.blueAccent,
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              log(4);
+            },
+            child: Icon(Icons.plus_one_outlined)),
+        backgroundColor: Colors.white70,
+        drawer: Container(
+          width: 300,
+          color: Colors.white70,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("Hello, Welcome back!"),
-            Text("Login to continue"),
+            Row(
+              children: [
+                Text("Hello, Welcome back!"),
+                Text("Login to continue"),
+              ],
+            ),
+            Row(
+              children: [
+                TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(hintText: "username"),
+                ),
+                TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(hintText: "password"),
+                )
+              ],
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Forgot password?"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Login"),
+            ),
+            Text("or login with"),
+            ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    "asset/image/ic_google.png",
+                    width: 20,
+                    height: 20,
+                  ),
+                  Text("login with google"),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset(
+                    "asset/image/ic_facebook.png",
+                    width: 20,
+                    height: 20,
+                  ),
+                  Text("login with facebook"),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Text("Don't have an account? "),
+                TextButton(onPressed: () {}, child: Text("Sign Up")),
+              ],
+            ),
           ],
-        )),
+        ),
       ),
     );
   }
